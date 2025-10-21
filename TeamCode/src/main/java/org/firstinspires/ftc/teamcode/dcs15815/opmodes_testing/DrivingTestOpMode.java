ab991bde3eeb681f2ecf93481c18457904d914b6
mmcr <thew.roberts@gmail.com>
2025-10-21 17:10:04 -0400

    OpMode and robot class added for drive simple drive testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes_testing/DrivingTestOpMode.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.dcs15815.opmodes_testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Driving Test", group = "Testing")
+public class DrivingTestOpMode extends LinearOpMode {
+    private ComplexDriveRobot bot;
+
+    @Override
+    public void runOpMode() {
+
+        bot = new ComplexDriveRobot(hardwareMap);
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+            bot.drive(-1 * gamepad1.left_stick_y, (gamepad1.right_trigger - gamepad1.left_trigger), gamepad1.right_stick_x);
+
+
+        }
+
+    }
+
+}
