9c3b683d26e7801279d1d466e99219b8e92f6772
mmcr <thew.roberts@gmail.com>
2025-12-02 21:23:33 -0500

    Renamed navigation classes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/DefenderFramework/DefenderUtilities/DefenderPIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/DefenderFramework/DefenderUtilities/DefenderPIDController.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.dcs15815.DefenderFramework.DefenderUtilit
 
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.dcs15815.DecodeBot.DecodeNavigation;
+import org.firstinspires.ftc.teamcode.dcs15815.DecodeBot.DecodeNavigationOTOS;
 
 public class DefenderPIDController {
     boolean usesAngles = false;
@@ -27,7 +27,7 @@ public class DefenderPIDController {
     public double calculate(double referencePosition, double currentPosition) {
         double error;
         if (usesAngles) {
-            error = DecodeNavigation.calculateYawError(currentPosition, referencePosition);
+            error = DecodeNavigationOTOS.calculateYawError(currentPosition, referencePosition);
         } else {
             error = referencePosition - currentPosition;
         }
