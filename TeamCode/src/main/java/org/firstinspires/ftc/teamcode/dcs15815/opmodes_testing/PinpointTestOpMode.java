63f0d9df57eea9b1026157e2c6b671d3be967c71
mmcr <thew.roberts@gmail.com>
2025-12-06 08:48:01 -0500

    Changes for testing.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes_testing/PinpointTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes_testing/PinpointTestOpMode.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.dcs15815.DecodeBot.DecodeBot;
 import org.firstinspires.ftc.teamcode.dcs15815.DecodeBot.DecodeConfiguration;
 
@@ -14,14 +15,19 @@ import org.firstinspires.ftc.teamcode.dcs15815.DecodeBot.DecodeConfiguration;
 public class PinpointTestOpMode extends LinearOpMode {
     DecodeBot bot;
 
-
+    static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.INCH, 12, 0, AngleUnit.DEGREES, 0);
 
     @Override
     public void runOpMode() {
 
+
         bot = new DecodeBot(hardwareMap, DecodeConfiguration.class, telemetry);
         waitForStart();
 
+        while (!bot.navigation.driveToFromCurrent(TARGET_1, 1)) {
+
+        }
+        bot.drivetrain.stopDriving();
 
 
     }
