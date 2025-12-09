dda92654f583fabf5531c66e71436a2d5f08d674
mmcr <thew.roberts@gmail.com>
2025-12-09 07:38:10 -0500

    Added telemetry.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes_testing/PinpointTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes_testing/PinpointTestOpMode.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.dcs15815.opmodes_testing;
 
-import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -15,17 +13,33 @@ import org.firstinspires.ftc.teamcode.dcs15815.DecodeBot.DecodeConfiguration;
 public class PinpointTestOpMode extends LinearOpMode {
     DecodeBot bot;
 
-    static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.INCH, 12, 0, AngleUnit.DEGREES, 0);
+    static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.INCH, 0, 24, AngleUnit.DEGREES, 90);
 
     @Override
     public void runOpMode() {
 
 
         bot = new DecodeBot(hardwareMap, DecodeConfiguration.class, telemetry);
+
+        while (!isStarted() && !opModeIsActive()) {
+            bot.navigation.updatePosition();
+
+            telemetry.addData("x", bot.navigation.getPosition().getX(DistanceUnit.INCH));
+            telemetry.addData("y", bot.navigation.getPosition().getY(DistanceUnit.INCH));
+            telemetry.addData("h", bot.navigation.getPosition().getHeading(AngleUnit.DEGREES));
+            telemetry.addData("err", bot.navigation.totalError);
+            telemetry.update();
+        }
+
         waitForStart();
 
-        while (!bot.navigation.driveToFromCurrent(TARGET_1, 1)) {
-
+        while (!bot.navigation.driveToFromCurrent(TARGET_1, 1) && opModeIsActive()) {
+            telemetry.addData("x", bot.navigation.getPosition().getX(DistanceUnit.INCH));
+            telemetry.addData("y", bot.navigation.getPosition().getY(DistanceUnit.INCH));
+            telemetry.addData("h", bot.navigation.getPosition().getHeading(AngleUnit.DEGREES));
+            telemetry.addData("err", bot.navigation.totalError);
+            telemetry.update();
+            bot.navigation.updatePosition();
         }
         bot.drivetrain.stopDriving();
 
